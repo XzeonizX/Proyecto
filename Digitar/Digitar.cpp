0f61317686f81d0ceb0c0b71a0107d53df8b6204
XzeonizX <n.cedeno@utp.edu.co>
2020-11-09 15:19:44 -0500

    09/11/2020

--- Digitar/Digitar.cpp
+++ Digitar/Digitar.cpp
@@ -8,11 +8,12 @@ int main (){
     int NuevoDato, Numero;
     cout<<"Digite: 1. Si desea ingresar nuevos datos 2. Si desea hacer una busqueda ";
     cin>>NuevoDato;
-    if (NuevoDato = 1){
+    if (NuevoDato == 1){
         int Numero;
         cout<<"\nDigite el numero que quiere asignar";
         cin>>Numero;
-        } else {
+        } else if (NuevoDato == 2){
+                          
             int dato;
             cout<<"digite el numero que le gustaria buscar";
             cin>>dato;
